f9c987590bd381d1083fc06a74ee84318410e0aa
umutalacam <umutcanalacam@gmail.com>
2021-03-21 18:03:22 +0300

    Update browser window styles

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const {app, BrowserWindow} = require("electron")
 
 function createWindow () {
     const win = new BrowserWindow({
-        width : 800,
-        height : 650,
+        width : 1000,
+        height : 720,
         webPreferences : {
             nodeIntegration: true,
             webviewTag: true
